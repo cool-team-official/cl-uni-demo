40061a0c465791b2fba2da8f85057d680a9ad3ab
icssoa <615206459@qq.com>
2020-11-25 11:43:24 +0800

    优化

---
+++ src/pages/demo/advanced/calendar.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="demo-calendar">
+		<cl-card label="默认">
+			<cl-button @tap="open">打开</cl-button>
+			<calendar ref="calendar" />
+		</cl-card>
+	</view>
+</template>
+
+<script>
+import Calendar from "@/components/calendar";
+
+export default {
+	components: {
+		Calendar
+	},
+
+	methods: {
+		open() {
+			this.$refs.calendar.open();
+		}
+	}
+};
+</script>
