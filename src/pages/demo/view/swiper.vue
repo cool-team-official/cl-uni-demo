3bbe3eb8f8e9569e8936aa893609599632d5cc36
icssoa <615206459@qq.com>
2020-12-31 17:10:59 +0800

    优化

--- src/pages/demo/view/swiper.vue
+++ src/pages/demo/view/swiper.vue
@@ -3,6 +3,14 @@
 		<cl-card label="默认">
 			<cl-swiper :list="list"> </cl-swiper>
 		</cl-card>
+
+		<cl-card label="卡片式">
+			<cl-swiper type="card" :list="list"></cl-swiper>
+		</cl-card>
+
+		<cl-card label="链式">
+			<cl-swiper type="chain" :list="list"></cl-swiper>
+		</cl-card>
 	</view>
 </template>
 
