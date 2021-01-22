3210af24dc820aa6ff1980d40d361f92ae9d7cd2
icssoa <615206459@qq.com>
2021-01-22 17:18:52 +0800

    1

--- src/cl-uni/components/cl-list-index/cl-list-index.vue
+++ src/cl-uni/components/cl-list-index/cl-list-index.vue
@@ -2,26 +2,12 @@
 	<view class="cl-li">
 		<!-- 搜索栏 -->
 		<view class="cl-li__search" v-if="filterable">
-			<cl-input
-				v-model="keyWord"
-				:border="false"
-				prefix-icon="cl-icon-search"
-				round
-				placeholder="搜索关键字"
-				clearable
-				@search="onSearch"
-			></cl-input>
+			<cl-input v-model="keyWord" :border="false" prefix-icon="cl-icon-search" round placeholder="搜索关键字" clearable @search="onSearch"></cl-input>
 		</view>
 
 		<!-- 滚动视图 -->
-		<scroll-view
-			class="cl-li__scroller"
-			scroll-y
-			enable-back-to-top
-			scroll-with-animation
-			:scroll-into-view="`index-${label}`"
-			@scroll="onScroll"
-		>
+		<scroll-view class="cl-li__scroller" scroll-y enable-back-to-top scroll-with-animation :scroll-into-view="`index-${label}`"
+		 @scroll="onScroll">
 			<!-- 追加内容到头部 -->
 			<slot name="prepend"></slot>
 
@@ -37,7 +23,7 @@
 					</view>
 					<!-- 数据列表 -->
 					<view class="cl-li__container">
-						<template v-for="(item2, index2) in item.children">
+						<view v-for="(item2, index2) in item.children">
 							<!-- 内容插槽 -->
 							<slot :item="item2" :parent="item">
 								<view class="cl-li__item" :key="index2" @tap="selectRow(item2)">
@@ -45,7 +31,7 @@
 									<cl-text :margin="[0, 0, 0, 20]" :value="item2.name"></cl-text>
 								</view>
 							</slot>
-						</template>
+						</view>
 					</view>
 				</view>
 			</template>
@@ -58,12 +44,8 @@
 		<view class="cl-li__bar">
 			<view class="cl-li__bar-list" @touchmove.stop.prevent="barMove" @touchend="barEnd">
 				<template v-for="(item, index) in flist">
-					<view
-						:class="['cl-li__bar-block', curr.label == item.label ? 'is-active' : '']"
-						:key="index"
-						:id="index"
-						@touchstart.stop.prevent="toRow(item)"
-					>
+					<view :class="['cl-li__bar-block', curr.label == item.label ? 'is-active' : '']" :key="index" :id="index"
+					 @touchstart.stop.prevent="toRow(item)">
 						<text>{{ item.label }}</text>
 					</view>
 				</template>
@@ -76,187 +58,191 @@
 </template>
 
 <script>
-import { last } from "../../utils";
+	import {
+		last
+	} from "../../utils";
 
-/**
- * list-index 索引列表
- * @description 索引列表, 支持自定义内容
- * @tutorial https://docs.cool-js.com/uni/components/advanced/list-index.html
- * @property {Number} index 序号
- * @property {Array} list 列表数据
- * @property {Boolean} filterable 是否带有过滤栏，默认true
- * @property {String} nodeKey 节点关键字，默认id
- * @event {Function} change 发生改变时触发
- * @event {Function} search 搜索时触发，function(keyword)
- * @event {Function} select 选择行时触发，function(item)
- * @example 见教程
- */
+	/**
+	 * list-index 索引列表
+	 * @description 索引列表, 支持自定义内容
+	 * @tutorial https://docs.cool-js.com/uni/components/advanced/list-index.html
+	 * @property {Number} index 序号
+	 * @property {Array} list 列表数据
+	 * @property {Boolean} filterable 是否带有过滤栏，默认true
+	 * @property {String} nodeKey 节点关键字，默认id
+	 * @event {Function} change 发生改变时触发
+	 * @event {Function} search 搜索时触发，function(keyword)
+	 * @event {Function} select 选择行时触发，function(item)
+	 * @example 见教程
+	 */
 
-export default {
-	name: "cl-list-index",
+	export default {
+		name: "cl-list-index",
 
-	props: {
-		// 序号
-		index: {
-			type: Number,
-			default: 0,
-		},
-		// 列表数据
-		list: Array,
-		// 是否带有过滤栏
-		filterable: {
-			type: Boolean,
-			default: true,
-		},
-		// 节点关键字
-		nodeKey: {
-			type: String,
-			default: "id",
-		},
-	},
-
-	data() {
-		return {
-			keyWord: "",
-			label: "",
-			alert: false,
-			curr: {},
-			bar: {
-				top: 0,
-				itemH: 0,
+		props: {
+			// 序号
+			index: {
+				type: Number,
+				default: 0,
 			},
-			tops: [],
-		};
-	},
-
-	watch: {
-		index: {
-			immediate: true,
-			handler: "setData",
-		},
-
-		list: {
-			handler: "doLayout",
-		},
-
-		curr: {
-			handler(val) {
-				this.$emit("change", val);
+			// 列表数据
+			list: Array,
+			// 是否带有过滤栏
+			filterable: {
+				type: Boolean,
+				default: true,
+			},
+			// 节点关键字
+			nodeKey: {
+				type: String,
+				default: "id",
 			},
 		},
-	},
 
-	computed: {
-		flist() {
-			const match = (d) => (d ? d.name.includes(this.keyWord) : false);
-
-			return this.list
-				.filter((e) => e.children && e.children.find(match))
-				.map((e) => {
-					e.children = e.children.filter(match);
-					return e;
-				});
-		},
-	},
-
-	mounted() {
-		this.doLayout();
-	},
-
-	methods: {
-		onSearch(val) {
-			this.$emit("search", val);
+		data() {
+			return {
+				keyWord: "",
+				label: "",
+				alert: false,
+				curr: {},
+				bar: {
+					top: 0,
+					itemH: 0,
+				},
+				tops: [],
+			};
 		},
 
-		onScroll(e) {
-			let top = e.detail.scrollTop;
-			let num = this.tops.filter((e) => top >= e - 60).length - 1;
+		watch: {
+			index: {
+				immediate: true,
+				handler: "setData",
+			},
 
-			if (num < 0) {
-				num = 0;
-			}
+			list: {
+				handler: "doLayout",
+			},
 
-			this.curr = this.list[num];
+			curr: {
+				handler(val) {
+					this.$emit("change", val);
+				},
+			},
 		},
 
-		// 选择行
-		selectRow(item) {
-			this.$emit("select", item);
-		},
+		computed: {
+			flist() {
+				const match = (d) => (d ? d.name.includes(this.keyWord) : false);
 
-		// 更新行数据，避免小程序等平台slot作用域异常问题
-		updateRow(id, key, data) {
-			this.list.map((e) => {
-				if (e.children) {
-					e.children.map((c) => {
-						if (c[this.nodeKey] == id) {
-							this.$set(c, key, data);
-						}
+				return this.list
+					.filter((e) => e.children && e.children.find(match))
+					.map((e) => {
+						e.children = e.children.filter(match);
+						return e;
 					});
+			},
+		},
+
+		mounted() {
+			this.doLayout();
+		},
+
+		methods: {
+			onSearch(val) {
+				this.$emit("search", val);
+			},
+
+			onScroll(e) {
+				let top = e.detail.scrollTop;
+				let num = this.tops.filter((e) => top >= e - 60).length - 1;
+
+				if (num < 0) {
+					num = 0;
 				}
-			});
+
+				this.curr = this.list[num];
+			},
+
+			// 选择行
+			selectRow(item) {
+				this.$emit("select", item);
+			},
+
+			// 更新行数据，避免小程序等平台slot作用域异常问题
+			updateRow(id, key, data) {
+				this.list.map((e) => {
+					if (e.children) {
+						e.children.map((c) => {
+							if (c[this.nodeKey] == id) {
+								this.$set(c, key, data);
+							}
+						});
+					}
+				});
+			},
+
+			// 根据序号设置选择数据
+			setData(index) {
+				this.curr = this.list[index] || {};
+				this.label = this.curr.label;
+			},
+
+			toRow(e) {
+				this.alert = true;
+				this.curr = e;
+			},
+
+			barMove(e) {
+				const max = this.list.length;
+				let index = parseInt((e.touches[0].clientY - this.bar.top) / this.bar.itemH);
+
+				if (index >= max) {
+					index = max - 1;
+				}
+
+				if (index < 0) {
+					index = 0;
+				}
+
+				this.curr = this.list[index];
+			},
+
+			barEnd() {
+				this.label = this.curr.label;
+				this.alert = false;
+			},
+
+			doLayout() {
+				this.$nextTick(() => {
+					// 获取索引栏大小
+					uni.createSelectorQuery()
+						.in(this)
+						.select(`.cl-li__bar-list`)
+						.boundingClientRect((res) => {
+							this.bar.top = res.top;
+							this.bar.itemH = parseInt(res.height / this.list.length);
+						})
+						.exec();
+
+					// 获取当前距离顶部的高度
+					uni.createSelectorQuery()
+						.in(this)
+						.select(".cl-li")
+						.boundingClientRect((res) => {
+							// 获取每项距离顶部的高度
+							uni.createSelectorQuery()
+								.in(this)
+								.selectAll(".cl-li__header")
+								.fields({
+									rect: true
+								})
+								.exec((d) => {
+									this.tops = d[0].map((e) => e.top - res.top);
+								});
+						})
+						.exec();
+				});
+			},
 		},
-
-		// 根据序号设置选择数据
-		setData(index) {
-			this.curr = this.list[index] || {};
-			this.label = this.curr.label;
-		},
-
-		toRow(e) {
-			this.alert = true;
-			this.curr = e;
-		},
-
-		barMove(e) {
-			const max = this.list.length;
-			let index = parseInt((e.touches[0].clientY - this.bar.top) / this.bar.itemH);
-
-			if (index >= max) {
-				index = max - 1;
-			}
-
-			if (index < 0) {
-				index = 0;
-			}
-
-			this.curr = this.list[index];
-		},
-
-		barEnd() {
-			this.label = this.curr.label;
-			this.alert = false;
-		},
-
-		doLayout() {
-			this.$nextTick(() => {
-				// 获取索引栏大小
-				uni.createSelectorQuery()
-					.in(this)
-					.select(`.cl-li__bar-list`)
-					.boundingClientRect((res) => {
-						this.bar.top = res.top;
-						this.bar.itemH = parseInt(res.height / this.list.length);
-					})
-					.exec();
-
-				// 获取当前距离顶部的高度
-				uni.createSelectorQuery()
-					.in(this)
-					.select(".cl-li")
-					.boundingClientRect((res) => {
-						// 获取每项距离顶部的高度
-						uni.createSelectorQuery()
-							.in(this)
-							.selectAll(".cl-li__header")
-							.fields({ rect: true })
-							.exec((d) => {
-								this.tops = d[0].map((e) => e.top - res.top);
-							});
-					})
-					.exec();
-			});
-		},
-	},
-};
+	};
 </script>
--- src/pages/demo/advanced/list-index.vue
+++ src/pages/demo/advanced/list-index.vue
@@ -1,181 +1,145 @@
 <template>
-	<cl-list-index
-		ref="list-index"
-		:index="active"
-		:list="list"
-		@search="onSearch"
-		@select="onSelect"
-		@change="onChange"
-	>
+	<cl-list-index ref="list-index" :index="active" :list="list" @search="onSearch" @select="onSelect" @change="onChange">
 	</cl-list-index>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			active: 1,
-			list: [
-				{
-					label: "A",
-					children: [
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/1.jpg",
-							name: "阿雪"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/3.jpg",
-							name: "阿良"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/2.jpg",
-							name: "阿绵"
-						}
-					]
-				},
-				{
-					label: "C",
-					children: [
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/5.jpg",
-							name: "陈杨"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/6.jpg",
-							name: "陈飞"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/7.jpg",
-							name: "陈品如"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/4.jpg",
-							name: "陈逸"
-						}
-					]
-				},
-				{
-					label: "D",
-					children: [
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/10.jpg",
+	export default {
+		data() {
+			return {
+				active: 1,
+				list: [{
+						label: "A",
+						children: [{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/1.jpg",
+								name: "阿雪"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/3.jpg",
+								name: "阿良"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/2.jpg",
+								name: "阿绵"
+							}
+						]
+					},
+					{
+						label: "C",
+						children: [{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/5.jpg",
+								name: "陈杨"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/6.jpg",
+								name: "陈飞"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/7.jpg",
+								name: "陈品如"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/4.jpg",
+								name: "陈逸"
+							}
+						]
+					},
+					{
+						label: "D",
+						children: [{
+							avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/10.jpg",
 							name: "道天"
-						}
-					]
-				},
-				{
-					label: "H",
-					children: [
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/9.jpg",
-							name: "韩琦"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/10.jpg",
-							name: "韩重"
-						}
-					]
-				},
-				{
-					label: "J",
-					children: [
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/3.jpg",
-							name: "江澄"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/7.jpg",
-							name: "江厌离"
-						}
-					]
-				},
-				{
-					label: "S",
-					children: [
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/2.jpg",
-							name: "苏三"
-						},
-						,
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/4.jpg",
-							name: "沈道"
-						}
-					]
-				},
-				{
-					label: "Z",
-					children: [
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/4.jpg",
-							name: "周蓓蓓"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/2.jpg",
-							name: "朱勇"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/3.jpg",
-							name: "朱鸣"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/1.jpg",
-							name: "张伟"
-						},
-						{
-							avatarUrl:
-								"https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/9.jpg",
-							name: "郑金榜"
-						}
-					]
-				}
-			]
-		};
-	},
-
-	methods: {
-		onSearch(v) {
-			console.log("search", iv);
+						}]
+					},
+					{
+						label: "H",
+						children: [{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/9.jpg",
+								name: "韩琦"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/10.jpg",
+								name: "韩重"
+							}
+						]
+					},
+					{
+						label: "J",
+						children: [{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/3.jpg",
+								name: "江澄"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/7.jpg",
+								name: "江厌离"
+							}
+						]
+					},
+					{
+						label: "S",
+						children: [{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/2.jpg",
+								name: "苏三"
+							}, ,
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/4.jpg",
+								name: "沈道"
+							}
+						]
+					},
+					{
+						label: "Z",
+						children: [{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/4.jpg",
+								name: "周蓓蓓"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/2.jpg",
+								name: "朱勇"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/3.jpg",
+								name: "朱鸣"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/1.jpg",
+								name: "张伟"
+							},
+							{
+								avatarUrl: "https://cool-comm.oss-cn-shenzhen.aliyuncs.com/show/imgs/chat/avatar/9.jpg",
+								name: "郑金榜"
+							}
+						]
+					}
+				]
+			};
 		},
 
-		onSelect(item) {
-			console.log("select", item);
-		},
+		methods: {
+			onSearch(v) {
+				console.log("search", iv);
+			},
 
-		onChange(item) {
-			console.log("change", item);
+			onSelect(item) {
+				console.log("select", item);
+			},
+
+			onChange(item) {
+				console.log("change", item);
+			}
 		}
-	}
-};
+	};
 </script>
 
 <style lang="scss">
-page {
-	// #ifdef H5
-	height: 100%;
-	// #endif
+	page {
+		// #ifdef H5
+		height: 100%;
+		// #endif
 
-	// #ifndef H5
-	height: 100vh;
-	// #endif
-	overflow: hidden;
-}
+		// #ifndef H5
+		height: 100vh;
+		// #endif
+		overflow: hidden;
+	}
 </style>
